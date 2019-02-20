60e00fe0472495beeb2fa7d58351c98d7745e14c
Rachel Wyatt <rw126@evansville.edu>
2019-02-20 22:32:15 +0000

    Automatically backed up by Learn

---
+++ app/models/song.rb
@@ -0,0 +1,6 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: true 
+  validates :released, inclusion: { in: [true, false] }
+  validates :artist_name, presence: true
+  
+end
