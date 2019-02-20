93180861951201917c0a2138b970a70815609a14
Rachel Wyatt <rw126@evansville.edu>
2019-02-20 22:34:07 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -35,6 +35,11 @@ class SongsController < ApplicationController
     end
   end
 
+  def destroy
+    Song.find(params[:id]).destroy
+    redirect_to songs_url
+  end
+
   private
   def song_params
     params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
